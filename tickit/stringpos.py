110f2a8b9c3a138f3fda50625a6c9ad6398eda3a
Kiyoshi Aman <kiyoshi.aman@gmail.com>
2014-12-25 15:38:38 -0600

    tickit.stringpos: Refactor over new tickit._tickit.

--- tickit/stringpos.py
+++ tickit/stringpos.py
@@ -1,64 +1,36 @@
 
 import tickit._tickit as tickit
 
-class StringPos(tickit.TickitStringPos):
-    """String position counters and limits thereunto.
-    """
-    def __init__(self):
-        self.bytes = self.codepoints = self.graphemes = self.columns = 0
-
+class StringPos(tickit.StringPos):
     @classmethod
     def zero(cls):
         self = cls()
+        self.bytes = self.codepoints = self.graphemes = self.columns = 0
+
+    @classmethod
+    def limit_bytes(cls, bytes):
+        self = cls.zero()
+        self.bytes = bytes
+
         return self
 
     @classmethod
-    def limit_bytes(cls, size):
-        self = cls()
-        self.bytes = size
-        self.codepoints = self.graphemes = self.columns = -1
+    def limit_codepoints(cls, codepoints):
+        self = cls.zero()
+        self.codepoints = codepoints
+
         return self
 
     @classmethod
-    def limit_codepoints(cls, size):
-        self = cls()
-        self.codepoints = size
-        self.bytes = self.graphemes = self.columns = -1
+    def limit_graphemes(cls, graphemes):
+        self = cls.zero()
+        self.graphemes = graphemes
+
         return self
 
     @classmethod
-    def limit_graphemes(cls, size):
-        self = cls()
-        self.graphemes = size
-        self.bytes = self.codepoints = self.columns = -1
+    def limit_columns(cls, columns):
+        self = cls.zero()
+        self.graphemes = graphemes
+
         return self
-
-    @classmethod
-    def limit_columns(cls, size):
-        self = cls()
-        self.columns = size
-        self.bytes = self.codepoints = self.graphemes = -1
-        return self
-
-    @property
-    def bytes(self):
-        return self.bytes
-
-    @property
-    def codepoints(self):
-        return self.codepoints
-
-    @property
-    def graphemes(self):
-        return self.graphemes
-
-    @property
-    def columns(self):
-        return self.columns
-
-def string_count(string, pos, limit):
-    return tickit.tickit_string_count(string, pos, limit)
-
-def string_countmore(string, pos, limit):
-    return tickit.tickit_string_countmore(string, pos, limit)
-
