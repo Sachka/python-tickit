65a74ac34c114db0461c1ec25ca3c4f47f0db7b4
Kiyoshi Aman <kiyoshi.aman@gmail.com>
2014-12-25 01:25:25 -0600

    tickit.rectset: Refactor over new tickit._tickit. MutableSequence → (Sized, Iterable)

--- tickit/rectset.py
+++ tickit/rectset.py
@@ -1,56 +1,54 @@
 
 try:
-    from collections.abc import MutableSequence
+    from collections.abc import Iterable, Sized
 except ImportError:
-    from collections import MutableSequence
+    from collections import Iterable, Sized
 
-from tickit.rect import Rect
+from tickit.rect import rect
 
 import tickit._tickit as tickit
 
-class RectSet(MutableSequence):
+class RectSet(Iterable, Sized):
     def __init__(self):
-        self._set = tickit.tickit_rectset_new()
+        self._set = tickit.rectset_new()
         self._rects = []
+
+    @property
+    def rects(self):
+        return list(self._rects)
 
     def __len__(self):
         return len(self._rects)
 
-    def __contains__(self, value):
-        return bool(tickit.tickit_rectset_contains(self._set, value._rect))
-
     def __iter__(self):
         return iter(self._rects)
 
+    def __contains__(self, value):
+        return self.contains(value)
+
+    def contains(self, value):
+        return tickit.rectset_contains(self._set, value._rect)
+
     def discard(self, value):
-        self.subtract(self, value)
-
-    def subtract(self, value):
-        tickit.tickit_rectset_subtract(self._set, value._rect)
-        self._update()
-
-    @property
-    def rects(self):
-        return self._rects
+        self.subtract(value)
 
     def _update(self):
-        count = tickit.tickit_rectset_rects(self._set)
-        rect_arr = tickit.TickitRect * count
-        rects = rect_arr()
-        newcount = tickit.tickit_rectset_get_rects(self._set, rects, count)
-        self._rects = [Rect(obj=x) for x in rects]
+        count = tickit.rectset_rects(self._set)
+        rects = (tickit.rect * count)()
+        newcount = tickit.rectset_get_rects(self._rect, rects, count)
+        self._rects = [Rect(rect.top, rect.left, rect.lines, rect.cols) for rect in rects]
+
+    def subtract(self, rect):
+        tickit.rectset_subtract(self._set, rect._rect)
+        self._update()
 
     def add(self, rect):
-        tickit.tickit_rectset_add(self._set, rect._rect)
+        tickit.rectset_add(self._set, rect._rect)
         self._update()
 
     def clear(self):
-        tickit.tickit_rectset_clear(self._set)
-        self._rects = []
+        tickit.rectset_clear(self._set)
+        self._update()
 
     def intersects(self, rect):
-        return bool(tickit.tickit_rectset_intersects(self._set, rect._rect))
-
-    def contains(self, rect):
-        return rect in self
-
+        return tickit.rectset_intersects(self._set, rect._rect)
